dc6c7d6032889cddae613c0189dd6e0cb2e35c50
Caleb Baird <79653447+calebbaird5@users.noreply.github.com>
2026-07-12 17:04:51 -0600

    cleanup

--- config/newFile.ts
+++
@@ -1,5 +0,0 @@
-if (!light.background || typeof light.background !== "string") {
-  throw new Error("Missing or invalid light theme background color.");
-} else if (!light.foreground || typeof light.foreground !== "string") {
-  throw new Error("Missing or invalid light theme foreground color.");
-}
